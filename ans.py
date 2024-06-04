638c8ea4b8c87a3f288aad8aae99bc46239df5ed
DarkSteelD <justjoke.exe@gmail.com>
2024-06-04 11:17:47 +0300

    Last upd

--- ans.py
+++ ans.py
@@ -2,7 +2,7 @@ from scipy import integrate
 import numpy as np
 from sympy.parsing.sympy_parser import parse_expr
 from sympy import symbols, lambdify
-with open('5.txt', 'r') as f:
+with open('C:\\Users\\DarkStell\\Documents\\GitHub\\Interpolaitcia\\5.txt', 'r') as f:
     str = f.readline()
     fun =  parse_expr(str)
     a = float(f.readline())
@@ -16,7 +16,7 @@ fun_callable = lambdify(x, fun, 'numpy')
 exact_integral = integrate.quad(fun_callable, a, b)[0]
 h = (b-a)/6
 ans_y = h/3*(fun.subs(x,nodes_y[0])+4*fun.subs(x,nodes_y[1])+2*fun.subs(x,nodes_y[2])+4*fun.subs(x,nodes_y[3])+2*fun.subs(x,nodes_y[4])+4*fun.subs(x,nodes_y[5])+fun.subs(x,nodes_y[6]))
-with open('результат.txt', 'w') as file:
+with open('C:\\Users\\DarkStell\\Documents\\GitHub\\Interpolaitcia\\результат.txt', 'w') as file:
     file.write(f"Точное значение интеграла: {exact_integral}")
     file.write(f"Численное значение интеграла: {ans_y} ")
     file.write(f"Отличие: {abs(exact_integral - ans_y)} ")
